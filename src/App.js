3673efd8828b496611babc33759d3531735163e0
gabrielsousas <gabriel.almeida006@gmail.com>
2023-02-26 17:12:15 -0300

    sorteia a palavra e mostra os underlines na tela

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import Letras from "./Letras";
+import Jogo from "./Jogo";
+import palavras from "./palavras";
+
 export default function App() {
+  const [palavraSorteada, setPalavraSorteada] = useState("");
   return (
     <div>
       <Letras></Letras>
+      <Jogo
+        palavraSorteada={palavraSorteada}
+        setPalavraSorteada={setPalavraSorteada}
+      ></Jogo>
     </div>
   );
 }
--- src/Jogo.js
+++ src/Jogo.js
@@ -1,3 +1,23 @@
-export default function Jogo(){
+import palavras from "./palavras";
+export default function Jogo({palavraSorteada,setPalavraSorteada}) {
+    return (
+        <div>
+          <button onClick={() => setPalavraSorteada(sorteiaPalavra(palavras))}>
+            Escolher Palavra
+          </button>
+          {palavraSorteada && (
+            <div>
+              {palavraSorteada.split("").map((letra, index) => (
+                <span key={index}>{letra === " " ? " " : "_"}</span>
+              ))}
+            </div>
+          )}
+        </div>
+      );
     
 }
+
+function sorteiaPalavra(palavras) {
+    const indice = Math.floor(Math.random() * palavras.length);
+    return palavras[indice];
+  }
