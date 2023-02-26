8201c4dea4d239554ea9c6aa1dce25fba3b35546
gabrielsousas <gabriel.almeida006@gmail.com>
2023-02-26 10:28:00 -0300

    componentização inicial e criação dos botões com as letras

---
+++ src/Letras.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Letras() {
+  const alfabeto = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+
+  return (
+    <div>
+      {alfabeto.map((a) => (
+        <button key={a}>{a}</button>
+      ))}
+    </div>
+  );
+}
